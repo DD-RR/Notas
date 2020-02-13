31b8b26ec566274b4258706effe74c4351c98154
Diego Rodríguez <mr.diego931@gmail.com>
2020-02-13 12:40:59 -0600

    Finalizando la clase

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
-const fs = require('fs')
+// Clase Creando Archivos y escribiendo en ellos
+// const fs = require('fs')
 
-//fs.writeFileSync('notas.txt', 'Mi creador es Diego Rodríguez. ')
+// //fs.writeFileSync('notas.txt', 'Mi creador es Diego Rodríguez. ')
 
-fs.appendFileSync('notas.txt', ' Fui creado en la Capacitación de NodeJS')
+// fs.appendFileSync('notas.txt', ' Fui creado en la Capacitación de NodeJS')
+
+// Clase importanto nuestros Propios archivos, desde un archivo diferente.
+//Aquí estan todos los archivos exportados.
+const getNota = require('./Notas')
+
+
+const msg = getNota ()
+ 
+console.log(msg)
