b9f86bbab6e73ea1a73e1870bd66f19749b055d4
Diego Rodríguez <mr.diego931@gmail.com>
2020-02-13 14:27:11 -0600

    Agregando la libreria de color al proyecto

--- app.js
+++ app.js
@@ -5,18 +5,24 @@
 
 // fs.appendFileSync('notas.txt', ' Fui creado en la Capacitación de NodeJS')
 
-// Clase importanto nuestros Propios archivos, desde un archivo diferente.
 //Aquí estan todos los archivos exportados.
-
-const validator = require('validator')
 const getNota = require('./Notas')
 
+// Clase importanto nuestros Propios archivos, desde un archivo diferente.
+    const msg = getNota ()
+    
+    console.log(msg)
 
-const msg = getNota ()
- 
-console.log(msg)
-console.log(validator.isEmail('algo@algo.com'))
-console.log(validator.isEmail('algo.com'))
-console.log(validator.isEmail('algo@'))
-console.log(validator.isURL('https://meat.io'))
-console.log(validator.isURL('https/meat.io'))
+// Clase de validaciones
+    // const validator = require('validator')
+
+    // console.log(validator.isEmail('algo@algo.com'))
+    // console.log(validator.isEmail('algo.com'))
+    // console.log(validator.isEmail('algo@'))
+    // console.log(validator.isURL('https://meat.io'))
+    // console.log(validator.isURL('https/meat.io'))
+
+// Clase de colores en el texto 
+    const chalk = require('chalk')
+
+    console.log(chalk.green('Exito'))
