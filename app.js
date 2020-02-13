3acabb608e95c1224db83535ee51830681deab9f
Diego Rodríguez <mr.diego931@gmail.com>
2020-02-13 17:35:19 -0600

    Aplicacion Notas v1

--- app.js
+++ app.js
@@ -37,4 +37,71 @@
 // Modulos Globales de npm
 // Intsalación de nodemon 
 
+// Inicio de la Aplicación de notas
 
+const chalk = require('chalk')
+const yargs = require('yargs')
+const getNotes = require('./Notas')
+
+// Configurando Manualmente la versión
+yargs.version('1.1.0')
+
+//Notas Agregar, Eliminar, leer, enumerar 
+
+// Creando el comando aggregar
+
+yargs.command({
+    command: 'add',
+    describe: 'Agregando una nueva Nota',
+    builder: {
+        title: {
+            describe: 'Titulo Nota',
+            demandOption: true,
+            type: 'string'
+        },
+        body : {
+            describe: 'Cuerpo de la Nota',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function (argv) {
+         console.log('Titulo: ', argv.title);
+         console.log('Cuerpo de la nota: ', argv.body);
+    }
+})
+
+
+// Creando el Comando remover
+
+yargs.command({
+    command: 'remove',
+    describe: 'Eliminando Notas',
+    handler: function () {
+        console.log('Eliminando Notas');
+    }
+})
+
+// Creando leer las notas
+
+yargs.command({
+    command: 'read',
+    describe: 'Leyendo las notas',
+    handler: function () {
+        console.log('Leyendo notas')
+    }
+})
+
+// Listando Notas
+
+yargs.command({
+    command: 'list',
+    describe: 'Listando las notas',
+    handler: function () {
+        console.log('Listando todas las notas')
+    }
+})
+
+yargs.parse()
+
+// console.log(yargs.argv)
